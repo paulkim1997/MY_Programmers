c502afa6e23a051c92ca2efbeea9602684de51e7
paulkim1997 <48825944+paulkim1997@users.noreply.github.com>
2021-01-28 10:42:50 +0900

    Update 문자열 내림차순으로 배치하기.cpp
    더 나은 풀이 추가

--- Level 1/문자열 내림차순으로 배치하기.cpp
+++ Level 1/문자열 내림차순으로 배치하기.cpp
@@ -1,6 +1,9 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+//기존의 문제 풀이:
+//문자열을 하나씩 잘라서 벡터에 넣고, 정렬
+//하나씩 불러와서 새로운 문자열 구성
 string solution(string s)
 {
     string answer = "";
@@ -18,6 +21,15 @@ string solution(string s)
     return answer;
 }
 
-int main(){
-    solution("qPoooyY");
+
+//새로운 문제 풀이:
+//compare사용해 정렬하자!
+//아스키 코드가 큰값->작은 순으로 정렬 (대문자는 원래 소문자보다 작다)
+bool cmp(const char &a, const char &b){
+    return a > b;
+}
+
+string solution(string s) {
+    sort(s.begin(),s.end(),cmp);
+    return s;
 }
