93abd90b7f4ecb13a08dc2ca499d9a152c80f510
paulkim1997 <48825944+paulkim1997@users.noreply.github.com>
2021-01-28 10:18:44 +0900

    UPDATE
    더 나은 풀이 추가

--- Level 1/3진법 뒤집기.cpp
+++ Level 1/3진법 뒤집기.cpp
@@ -1,6 +1,10 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+//기존 풀이 방식
+//10진법 -> 3진법 -> 앞뒤반전 -> 10진법
+//45    -> 1200 -> 0021    -> 7
+
 int solution(int n) {
     int answer = 0;
     vector<int> leftovers; //3으로 나눈 나머지들 저장
@@ -17,6 +21,22 @@ int solution(int n) {
     //앞뒤 반전 된 배열로 다시 10진수로
     for(int i = 0 ; i<leftovers.size(); i++) {
         answer+=leftovers[i]*pow(3,i);
+    }
+    return answer;
+}
+
+//새로운 풀이 방식
+//45   ->  1200 ->  0021   -> 7
+//0021이 45을 3으로 나눈 나머지들이다
+//숫자를 3으로 나누어주면서 나머지X3을 계속해준다
+int solution(int n)
+{
+    int answer = 0;
+    while (n > 0)
+    {
+        answer *= 3;
+        answer += n % 3;
+        n /= 3;
     }
     return answer;
 }
--- Level 1/가운데 글자 가져오기.cpp
+++ Level 1/가운데 글자 가져오기.cpp
@@ -12,8 +12,15 @@ string solution(string s) {
         answer1 =s[s.length()/2-1];
         answer2= s[s.length()/2];
         //원래 answer = s[s.length()/2-1] + s[s.length()/2]
-        //로 하려 했는데 왜 안될까요?
+        //로 하려 했는데 왜 안될까요? 정답: string형이 아닌 char형 이라서
         answer = answer1 + answer2;
     }
     return answer;
+}
+
+//한줄 문제 풀이법
+//substr 포지션: 문자열길이/2 짝수면 정중앙 전, 홀수면 정중앙
+//substr 길이 : 문자열이길의&1 을 하면 짝수는 false, 홀수만 true. 짝수면 2개, 홀수면 1개
+string solution(string s) {
+    string answer = s.substr((s.size()-1)/2, (s.size()&1) ? 1 : 2);
 }
